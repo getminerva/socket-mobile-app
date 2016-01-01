97dc45efb6d2c68648408a71586618ef2c1b113a
Joshua Paul Chan <joshuapaulchan@Joshuas-Air.home>
2015-12-31 20:40:33 -0500

    Change 'click' eventListener to 'touchstart' in BackButton
    300ms is soooo long. Also, included a few (comment-ed out) provisions for react-router 2.0.0 and new context thingamajigs.

--- src/assets/views/Utilities.jsx
+++ src/assets/views/Utilities.jsx
@@ -1,6 +1,9 @@
 var React = require('react');
 
 var BackButton = React.createClass({
+	// contextTypes: {
+	// 	'router': object.isRequired	// Context is a new thing in React, and React Router 2.0.0 is fine with using them
+	// },
 	getDefaultProps: function() {
 		return ({
 			'history': null		// Need the history object
@@ -8,8 +11,10 @@ var BackButton = React.createClass({
 	},
 	componentDidMount: function() {
 		// Attach return function to the button
-		document.querySelector('.button-return').addEventListener('click',
-			this.props.history.goBack);
+		document.querySelector('.button-return')
+			.addEventListener('touchstart', this.props.history.goBack);
+		// document.querySelector('.button-return')
+		// 	.addEventListener('touchstart', this.context.router.goBack);
 	},
 	render: function() {
 		return (
