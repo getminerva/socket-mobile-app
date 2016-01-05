d70b0a840b073f8f7aafac5efc6e4d9c73b80a30
Josh <joshpaulchan@gmail.com>
2016-01-05 00:07:10 -0500

    Change BackButton event handler to signal on 'touchend'; also, use context.router

--- src/assets/views/Utilities.jsx
+++ src/assets/views/Utilities.jsx
@@ -3,9 +3,9 @@ var Link = require('react-router').Link;
 var IndexLink = require('react-router').IndexLink;
 
 var BackButton = React.createClass({
-	// contextTypes: {
-	// 	'router': object.isRequired	// Context is a new thing in React, and React Router 2.0.0 is fine with using them
-	// },
+	contextTypes: {
+		'router': React.PropTypes.object	// Context is a new thing in React, and React Router 2.0.0 is fine with using them
+	},
 	getDefaultProps: function() {
 		return ({
 			'history': null		// Need the history object
@@ -13,10 +13,10 @@ var BackButton = React.createClass({
 	},
 	componentDidMount: function() {
 		// Attach return function to the button
-		document.querySelector('.button-return')
-			.addEventListener('touchstart', this.props.history.goBack);
 		// document.querySelector('.button-return')
-		// 	.addEventListener('touchstart', this.context.router.goBack);
+		// 	.addEventListener('touchend', this.props.history.goBack);
+		document.querySelector('.button-return')
+			.addEventListener('touchstart', this.context.router.goBack);
 	},
 	render: function() {
 		return (
