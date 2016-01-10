009a30a826ef7951d587a06ff2de1e080012cb4a
Josh <joshpaulchan@gmail.com>
2016-01-10 13:03:04 -0500

    Refactor SocketView
    1. Reorganize the beginning section
    2. Account for the case when a user interacts with the socket's name,
    enters it but there is not change -> won't ask them if they want to chang
    it and instead let's them go.
    3. Add a handler and confirm/code-setup for removing a socket. No backend
    support for the function, however.

--- src/assets/views/SocketView.jsx
+++ src/assets/views/SocketView.jsx
@@ -49,7 +49,6 @@ var SocketView = React.createClass({
 		});
 	},
 	changeProximity: function(ev) {
-		// TODO: Change the brightness of the socket [real-time]
 		var checked = ev.target.checked;
 		console.log(checked);
 		// [TODO] Try it via BT
@@ -73,17 +72,29 @@ var SocketView = React.createClass({
 	submitNameChange: function(ev) {
 		ev.preventDefault();
 		var newName = this.state.nickName;
-		// Verify name change is fine.
-		confirm('Are you sure to want to change \'' + this.state.originalName + '\' to \'' + newName + '\'?' , function (btn) {
+		if (newName != this.state.originalName) {
+			// Verify name change is fine.
+			confirm('Are you sure to want to change \'' + this.state.originalName + '\' to \'' + newName + '\'?' , function (btn) {
+				if (btn == 1) {
+					// If so, do it
+					//  [TODO] Update API
+					this.setState({'originalName': this.state.nickName});
+				} else {
+					// [TODO] Else, change it back to the original
+					this.setState({'nickName': this.originalName });
+				}
+			}, "Rename Socket", ["Yes", "No"]);
+		} else {
+			document.querySelector('.input-name').blur();
+		}
+	},
+	deleteSocket: function(ev) {
+		confirm('Are you sure you want to remove ' + this.state.originalName + '? This action cannot be undone.' function(btn) {
+			// TODO
 			if (btn == 1) {
-				// If so, do it
-				//  [TODO] Update API
-				this.setState({'originalName': this.state.nickName});
-			} else {
-				// [TODO] Else, change it back to the original
-				this.setState({'nickName': this.originalName });
+				console.log('Deleting ' + this.state.originalName + '...');
 			}
-		}, "Rename Socket", ["Yes", "No"]);
+		}, "Remove Socket", ['Remove', 'Cancel']);
 	},
 	componentWillMount: function() {
 		// Load the socket info from API
@@ -101,32 +112,34 @@ var SocketView = React.createClass({
 			console.log(error);
 		});
 	},
+	componentDidMount: function() {
+		document.querySelector('.button-close').addEventListener('touchend', this.deleteSocket);
+	},
 	render: function() {
 		return (
 			<div>
 				<Header>
 					<BackButton history={this.props.history}>Back</BackButton>
+					<button className='button button-close pull-right icon ion-close'></button>
 				</Header>
 				<div className='content has-header'>
 					<ul className='list'>
-						<li className='item'>
-							<div className='row'>
-								<div className='col'>
-								</div>
-								<div className='col col-75'>
-									<form onSubmit={this.submitNameChange}>
-										<label className='item item-input item-stacked-label'>
-											<span className='input-label'>
-												{this.state.uuid}
-											</span>
-											<input type='text'
-												value={this.state.nickName}
-												onChange={this.handleNameChange}
-											/>
-										</label>
-									</form>
-								</div>
-							</div>
+						<form className='item item-input' onSubmit={this.submitNameChange}>
+							<span className='input-label'>Name</span>
+							<input type='text'
+								className='input-name text-right'
+								value={this.state.nickName}
+								onChange={this.handleNameChange}
+							/>
+						</form>
+						<div className='item'>
+							MAC Address
+							<span className='item-note'>
+								{this.state.uuid}
+							</span>
+						</div>
+						<li className='item stable-bg'>
+							Brightness
 						</li>
 						<RangeItem
 							color='energized'
@@ -135,13 +148,19 @@ var SocketView = React.createClass({
 							value={this.state.curBrightness}
 							onChange={this.changeBrightness}
 						/>
-						<li className='item item-divider'></li>
 						<ToggleItem
-							color='energized'
+							color='balanced'
+							className='stable-bg'
 							checked={this.state.proximity}
-							onChange={this.changeProximity}
-							>Proximity Sense
+							onChange={this.changeProximity}>
+							Proximity Sense
 						</ToggleItem>
+						<label className='item item-input'>
+							<span className='input-label'>
+								Activation distance
+							</span>
+							<input type='number' size='3'/>
+						</label>
 					</ul>
 				</div>
 			</div>
