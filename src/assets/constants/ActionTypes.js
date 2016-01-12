fe6be246b773c936de28364b0fedb16a591c16b3
joshpaulchan <joshuapaulchan@gmail.com>
2016-01-11 21:41:56 -0500

    Start sockets action types
    I started writing the different actiontypes that can affect a 'sockets' state.

---
+++ src/assets/constants/ActionTypes.js
@@ -0,0 +1,10 @@
+const constants = {
+    Sockets: {
+        loadSockets:    'LOAD_SOCKETS',
+        addSocket:      'ADD_SOCKET',
+        createSocket:   'CREATE_SOCKET',
+        deleteSocket:   'DELETE_SOCKET'
+    }
+};
+
+module.exports = constants;
